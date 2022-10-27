de7acc11bce00aadc60ecceb51eeb500035e9ee5
rosario polito <rosario.polito@gmx.de>
2022-10-27 19:05:36 +0200

    adding "post" method

--- main.go
+++ main.go
@@ -1,17 +1,31 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
+
+type UserCredentials struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Hello Via Web")
 }
 
 func authHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Hello Via Web")
+	var userCredentials UserCredentials
+	err := json.NewDecoder(r.Body).Decode(&userCredentials)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	fmt.Printf("username: %s, password: %s \n", userCredentials.Username, userCredentials.Password)
+
 }
 
 func main() {
