dc0553b4f59f0a8946f6fe613f0ac335a2bd55bd
rosario polito <rosario.polito@gmx.de>
2022-10-27 20:36:32 +0200

    moving handler to different package

---
+++ authentication/handler.go
@@ -0,0 +1,36 @@
+package authentication
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+)
+
+type UserCredentials struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+func AuthHandler(w http.ResponseWriter, r *http.Request) {
+	var userCredentials UserCredentials
+	err := json.NewDecoder(r.Body).Decode(&userCredentials)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if userCredentials.Username == "someUsername" && userCredentials.Password == "somePassword" {
+		exp := time.Now().Add(10 * time.Minute)
+		jwt, err := GenerateJwt(userCredentials.Username, "admin", exp)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		w.Write([]byte(jwt))
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+
+	w.WriteHeader(http.StatusUnauthorized)
+}
--- main.go
+++ main.go
@@ -1,47 +1,16 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-	"time"
 
 	"github.com/rosariop/go-web/authentication"
 )
 
-type UserCredentials struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
-}
-
-func authHandler(w http.ResponseWriter, r *http.Request) {
-	var userCredentials UserCredentials
-	err := json.NewDecoder(r.Body).Decode(&userCredentials)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	if userCredentials.Username == "someUsername" && userCredentials.Password == "somePassword" {
-		exp := time.Now().Add(10 * time.Minute)
-		jwt, err := authentication.GenerateJwt(userCredentials.Username, "admin", exp)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-
-		w.Write([]byte(jwt))
-		w.WriteHeader(http.StatusOK)
-		return
-	}
-
-	w.WriteHeader(http.StatusUnauthorized)
-}
-
 func main() {
 
-	http.HandleFunc("/authenticate", authHandler)
+	http.HandleFunc("/authenticate", authentication.AuthHandler)
 
 	fmt.Println("Starting server on Port 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
