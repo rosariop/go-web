0ece307486a7715ea86e00c4a4a4973acdd48a8e
rosario polito <rosario.polito@gmx.de>
2022-10-28 19:40:37 +0200

    adding time for testing

--- authentication/validator.go
+++ authentication/validator.go
@@ -3,9 +3,10 @@ package authentication
 import (
 	"fmt"
 	"strings"
+	"time"
 )
 
-func validate(bearer string) bool {
+func validate(bearer string, t time.Time) bool {
 	s := strings.Split(bearer, " ")
 	tokenString := s[1]
 
--- authentication/validator_test.go
+++ authentication/validator_test.go
@@ -1,14 +1,18 @@
 package authentication
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
 func TestVerification(t *testing.T) {
 	// prepare
 	expected := true
 	bearer := "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRob3JpemVkIjp0cnVlLCJleHAiOiIyMDE5LTAxLTAxVDAwOjAwOjAwWiIsInJvbGVzIjpbImFkbWluIl0sInVzZXIiOiJzb21lVXNlcm5hbWUifQ.L5p5s_28DxnyH08l9iGJFAKqMI2KeaI6fFuJe9K2tdY"
+	var now = func() time.Time { return time.Date(2019, 1, 1, 0, 0, 0, 0, time.UTC) }
 
 	// action
-	actual := validate(bearer)
+	actual := validate(bearer, now())
 
 	//assert
 	if actual != expected {
