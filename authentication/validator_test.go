8f4a466dbda2cd045d0d8f2e7bfbaa165858df27
Rosario Polito <rosario.polito@gmx.de>
2022-10-30 22:54:00 +0100

    commenting out test

--- authentication/validator_test.go
+++ authentication/validator_test.go
@@ -24,3 +24,20 @@ func TestVerification(t *testing.T) {
 		t.FailNow()
 	}
 }
+
+/*
+func TestVerificationExpiredToken(t *testing.T) {
+	// prepare
+	bearer := "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJhdXRob3JpemVkIjp0cnVlLCJleHAiOjAsInJvbGVzIjpbImFkbWluIl0sInVzZXIiOiJzb21lVXNlcm5hbWUifQ.EYi6AVwMoXzj4cJBvD5AQ8qo-AvXEJH2UjFUflyOsMMPo6cbLmINrmDjrBUfcojt8Nzu8uMWKp7V9AfQyf7r2g"
+
+	// action
+	actual := validate(bearer)
+
+	//assert
+
+	if actual {
+		fmt.Println("")
+		t.FailNow()
+	}
+}
+*/
