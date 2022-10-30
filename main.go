cbd12b926e0ccf03802f229c55728770d11e5520
Rosario Polito <rosario.polito@gmx.de>
2022-10-30 22:41:06 +0100

    getting correct config for stage

--- main.go
+++ main.go
@@ -4,11 +4,22 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/rosariop/go-web/authentication"
 )
 
 func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("no stage given")
+		panic("no stage given")
+	}
+
+	if os.Args[3] == "test" {
+		godotenv.Load()
+	}
+
 	http.HandleFunc("/authenticate", authentication.AuthHandler)
 
 	fmt.Println("Starting server on Port 8080")
