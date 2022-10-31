d4086a6570b2467c978acf82b2427d1d6fabf4b2
Rosario Polito <rosario.polito@gmx.de>
2022-10-31 20:52:48 +0100

    adding test case with expired token

--- authentication/validator_test.go
+++ authentication/validator_test.go
@@ -25,9 +25,24 @@ func TestVerification(t *testing.T) {
 	}
 }
 
-func TestVerificationExpiredToken(t *testing.T) {
+func TestVerificationWrongFormat(t *testing.T) {
 	// prepare
 	bearer := "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJhdXRob3JpemVkIjp0cnVlLCJleHAiOjAsInJvbGVzIjpbImFkbWluIl0sInVzZXIiOiJzb21lVXNlcm5hbWUifQ.EYi6AVwMoXzj4cJBvD5AQ8qo-AvXEJH2UjFUflyOsMMPo6cbLmINrmDjrBUfcojt8Nzu8uMWKp7V9AfQyf7r2g"
+
+	// action
+	actual := validate(bearer)
+
+	//assert
+
+	if actual {
+		fmt.Println("")
+		t.FailNow()
+	}
+}
+
+func TestVerificationExpiredToken(t *testing.T) {
+	// prepare
+	bearer := "Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJhdXRob3JpemVkIjp0cnVlLCJleHAiOjAsInJvbGVzIjpbImFkbWluIl0sInVzZXIiOiJzb21lVXNlcm5hbWUifQ.EYi6AVwMoXzj4cJBvD5AQ8qo-AvXEJH2UjFUflyOsMMPo6cbLmINrmDjrBUfcojt8Nzu8uMWKp7V9AfQyf7r2g"
 
 	// action
 	actual := validate(bearer)
